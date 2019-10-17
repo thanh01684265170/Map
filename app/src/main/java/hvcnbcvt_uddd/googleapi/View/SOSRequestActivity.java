41a60990599eab9f075396b1ad1c941786a3fe3b
Vu Duc Chinh <vu.duc.chinh@sun-asterisk.com>
2019-10-17 11:20:33 +0700

    Login and Signup profile and send sos request xml

---
+++ app/src/main/java/hvcnbcvt_uddd/googleapi/View/SOSRequestActivity.java
@@ -0,0 +1,16 @@
+package hvcnbcvt_uddd.googleapi.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hvcnbcvt_uddd.googleapi.R;
+
+public class SOSRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sosrequest);
+    }
+}
